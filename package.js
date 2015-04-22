2758aaff5a7e199c64ea0b9c8aa45f56053636fe
phirumit <simphirum@gmail.com>
2015-04-22 17:01:24 +0700

    first commit

---
+++ package.js
@@ -0,0 +1,23 @@
+Package.describe({
+  name: 'phirum:round-khmer-currency',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'round khmer currency (Riel Currency:?)',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/phirumit/round-khmer-currency.git',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.export('roundRielCurrency');
+  api.addFiles('round-khmer-currency.js');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('phirum:round-khmer-currency');
+  api.addFiles('round-khmer-currency-tests.js');
+});
---
+++ round-khmer-currency.js
@@ -0,0 +1,12 @@
+// Write your package code here!
+roundRielCurrency = function (value) {
+    var returnValue = 0;
+    if (value > 0) {
+        returnValue = value % 100 >= 50 ? value + (100 - (value % 100)) : value - (value % 100);
+    } else if (value < 0) {
+        var absRK = Math.abs(value);
+        returnValue = absRK % 100 > 50 ? value - (100 - (absRK % 100)) : value + (absRK % 100);
+    }
+    return returnValue;
+
+};
