44e6a737adc414ce224e1a22c44cf1fa6c50fb46
phirum <phirumsim@gmail.com>
2015-04-29 09:35:58 +0700

    Update round-khmer-currency.js

--- round-khmer-currency.js
+++ round-khmer-currency.js
@@ -1,5 +1,6 @@
 // Write your package code here!
 roundRielCurrency = function (value) {
+     value=parseInt(value);
     var returnValue = 0;
     if (value > 0) {
         returnValue = value % 100 >= 50 ? value + (100 - (value % 100)) : value - (value % 100);
